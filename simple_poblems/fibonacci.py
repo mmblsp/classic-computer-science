2a3e41d5c8c16b4fb6e66396fa7602603fe25595
Anatoly Zhuchkov <a.zhuchkov@arenadata.io>
2022-10-06 00:13:16 +0300

    add func fibonacci_iteration_generator

--- simple_poblems/fibonacci.py
+++ simple_poblems/fibonacci.py
@@ -1,4 +1,5 @@
 """ functions for calculating fibonacci numbers """
+from typing import Generator
 from functools import lru_cache
 from typing import Dict
 
@@ -27,19 +28,34 @@ def fibonacci_automemoization(n: int) -> int:
         return n
     return fibonacci_automemoization(n-2) + fibonacci_automemoization(n-1)
 
+
 def fibonacci_iteration(n: int) -> int:
     """iteration method"""
-    if n == 0: return n
-    last: int =0
-    next: int =1
+    if n == 0:
+        return n
+    last: int = 0
+    next: int = 1
     for _ in range(1, n):
         last, next = next, last + next
     return next
-    
+
+
+def fibonacci_iteration_generator(n: int) -> Generator[int, None, None]:
+    """generation func iteration method"""
+    yield 0
+    if n > 0:
+        yield 1
+    last: int = 0
+    next: int = 1
+    for _ in range(1, n):
+        last, next = next, last + next
+        yield next
 
 
 if __name__ == '__main__':
     # print('recursive: {}'.format(fibonacci_recursive(40)))
-    print('memoization: {}'.format(fibonacci_memoization(40)))
-    print('automemoization: {}'.format(fibonacci_automemoization(40)))
-    print('iteration: {}'.format(fibonacci_iteration(40)))
+    # print('memoization: {}'.format(fibonacci_memoization(40)))
+    # print('automemoization: {}'.format(fibonacci_automemoization(40)))
+    # print('iteration: {}'.format(fibonacci_iteration(40)))
+    for i in fibonacci_iteration_generator(40):
+        print(i)
