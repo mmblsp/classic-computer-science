08532736b61122d63762d0616f9dfb60e9399b7a
Anatoly Zhuchkov <a.zhuchkov@arenadata.io>
2022-10-05 22:17:10 +0300

    add func fibonacci_automemoization

--- simple_poblems/fibonacci.py
+++ simple_poblems/fibonacci.py
@@ -1,4 +1,5 @@
 """ functions for calculating fibonacci numbers """
+from functools import lru_cache
 from typing import Dict
 
 
@@ -19,6 +20,15 @@ def fibonacci_memoization(n: int) -> int:
     return memo[n]
 
 
+@lru_cache(maxsize=None)
+def fibonacci_automemoization(n: int) -> int:
+    """auto-memoization using basic cases"""
+    if n < 2:
+        return n
+    return fibonacci_automemoization(n-2) + fibonacci_automemoization(n-1)
+
+
 if __name__ == '__main__':
     # print('recursive: {}'.format(fibonacci_recursive(40)))
     print('memo: {}'.format(fibonacci_memoization(40)))
+    print('memo: {}'.format(fibonacci_automemoization(40)))
