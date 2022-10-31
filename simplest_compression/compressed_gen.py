d8eeb61525ca5e3960169a1e45987633dda40838
Anatoly Zhuchkov <a.zhuchkov@arenadata.io>
2022-10-31 13:13:11 +0300

    complite compression gene code

--- simplest_compression/compressed_gen.py
+++ simplest_compression/compressed_gen.py
@@ -1,7 +1,7 @@
 """ trivial compression """
 
 
-class CompressedDene:
+class CompressedGene:
     """the nucleotide has four states - ASP E.
     Each unicode character takes 8 bits.
     Encoding in bits, allows you to compress information by 75%"""
@@ -9,17 +9,47 @@ class CompressedDene:
     def __init__(self, gene: str) -> None:
         self._compress(gene)
 
+    def __str__(self) -> str:
+        return self.decompress()
+
     def _compress(self, gene: str) -> None:
         self.bit_string: int = 1
         for nucleotide in gene.upper():
             self.bit_string <<= 2
-            if nucleotide == "A" :
+            if nucleotide == "A":
                 self.bit_string |= 0b00
-            elif nucleotide == "C" :
-                self.bit_string |= 0b00
-            elif nucleotide == "G" :
-                self.bit_string |= 0b00
-            elif nucleotide == "T" :
-                self.bit_string |= 0b00
+            elif nucleotide == "C":
+                self.bit_string |= 0b01
+            elif nucleotide == "G":
+                self.bit_string |= 0b10
+            elif nucleotide == "T":
+                self.bit_string |= 0b11
             else:
                 raise ValueError(f"Invalid Nucleotide:{nucleotide}")
+
+    def decompress(self) -> str:
+        """decompressed gene"""
+        gene: str = ""
+        for i in range(0, self.bit_string.bit_length() - 1, 2):
+            bits: int = self.bit_string >> i & 0b11
+            if bits == 0b00:
+                gene += "A"
+            elif bits == 0b01:
+                gene += "C"
+            elif bits == 0b10:
+                gene += "G"
+            elif bits == 0b11:
+                gene += "T"
+        return gene[::-1]
+
+
+if __name__ == "__main__":
+    from sys import getsizeof
+    import random
+    lst_nucleotide = ["A", "C", "G", "T"]
+    original: str = "".join(random.choices(lst_nucleotide, k=100))
+    compressed: CompressedGene = CompressedGene(original)
+    print(f"original is {getsizeof(original)} bytes")
+    print(f"compressed is {getsizeof(compressed.bit_string)} bytes")
+    print(
+        f"original and decompressed the same: {original == compressed.decompress()}")
